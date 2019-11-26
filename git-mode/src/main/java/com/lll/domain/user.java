a09349c47e60c4c46ba71ee966ca59a64d0cba42
fenrir <943247920@qq.com>
2019-11-26 14:16:24 +0800

    3

--- git-mode/src/main/java/com/lll/domain/user.java
+++ git-mode/src/main/java/com/lll/domain/user.java
@@ -1,10 +1,9 @@
 package com.lll.domain;
 
-import java.security.ProtectionDomain;
+
 
 public class user {
     public static void main(String[] args){
         System.out.println("hello git!");
     }
-
 }
