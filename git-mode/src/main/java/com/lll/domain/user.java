dc2922eb54f594cc7da40fab241643e398cb8c90
fenrir <943247920@qq.com>
2019-11-26 14:37:29 +0800

    cnm

--- git-mode/src/main/java/com/lll/domain/user.java
+++ git-mode/src/main/java/com/lll/domain/user.java
@@ -6,5 +6,6 @@ public class user {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("6");
+        System.out.println("xxxx");
     }
 }
